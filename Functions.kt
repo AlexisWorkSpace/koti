d9a0dfa42155feb532913616220e660bacf86fa1
Alexis Zeballos <97553981+AlexisWorkSpace@users.noreply.github.com>
2025-08-15 19:50:33 -0300

    Create Functions.kt

---
+++ Functions.kt
@@ -0,0 +1,24 @@
+package Functions
+
+fun <T : Comparable<T>> binarySearch(
+    list: List<T>,
+    condition: (T) -> Boolean,
+    exactMatch: Boolean = false
+): Int {
+    var l = 0
+    var r = list.size - 1
+    var result = -1
+
+    while (l <= r) {
+        val m = (l + r) / 2
+        val value = list[m]
+        if (condition(value)) {
+            result = m
+            if (exactMatch) return m
+            r = m - 1
+        } else {
+            l = m + 1
+        }
+    }
+    return result
+}
