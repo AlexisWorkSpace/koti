d9aa2541330f7384aa64c95cb015837f46c9a35e
Alexis Zeballos <97553981+AlexisWorkSpace@users.noreply.github.com>
2025-08-16 19:35:06 -0300

    Update Functions.kt

--- Functions.kt
+++ Functions.kt
@@ -22,3 +22,20 @@ fun <T : Comparable<T>> binarySearch(
     }
     return result
 }
+
+const val MAX = 100_500
+val isPrime = BooleanArray(MAX) { true }
+
+fun criba() {
+    isPrime[0] = false
+    isPrime[1] = false
+
+    for (p in 2 until MAX) {
+        if (isPrime[p]) {
+            for (m in 2 * p until MAX step p) {
+                isPrime[m] = false
+            }
+        }
+    }
+}
+
